12e8a4aa31c8d2129848b6f7bd0b0c1e4b56cfe5
Andre Menezes <dehhmenezes87@gmail.com>
2024-07-04 21:11:45 -0300

    fix: return api response

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,8 +11,29 @@ app.get('/registration', (req, res) => {
 
 app.post('/registration', (req, res) => {
   const { body } = req;
-  console.log('Dados recebidos:', body);
-  res.status(200).json({ message: 'Cadastro realizado com sucesso!' });
+
+  const commonFields = ['email', 'phone', 'client', 'password'];
+  const pfFields = ['name', 'cpf', 'birthdate'];
+  const pjFields = ['companyName', 'cnpj', 'openingDate'];
+
+  const missingCommonFields = commonFields.filter(field => !body[field]);
+
+  let missingSpecificFields = [];
+  if (body.client === 'PF') {
+    missingSpecificFields = pfFields.filter(field => !body[field]);
+  } else if (body.client === 'PJ') {
+    missingSpecificFields = pjFields.filter(field => !body[field]);
+  }
+
+  if (missingSpecificFields.length > 0 || missingCommonFields.length > 0) {
+    return res.status(400).json({ message: 'Não foi possível realizar o cadastro!' });
+  }
+
+  try {
+    return res.status(201).json({ message: 'Cadastro realizado com sucesso!'})
+  } catch (error) {
+    return res.status(400).json({ message: 'Não foi possível realizar o cadastro!' });
+  }
 });
 
 module.exports = app;
